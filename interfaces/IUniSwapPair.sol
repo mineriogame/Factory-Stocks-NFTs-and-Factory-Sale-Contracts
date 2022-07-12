8652be0bae4024ec15a45d8e308d44123c3ff8df
MinerioDev <dev@minerio.net>
2022-07-12 09:50:42 +0430

    First commit

---
+++ interfaces/IUniSwapPair.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IUniSwapPair {
+    function getReserves()
+        external
+        view
+        returns (
+            uint112,
+            uint112,
+            uint32
+        );
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+}
